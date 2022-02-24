d3c5b5641ed6348417489978a57713966979b7b9
Gaston Vareika <gvareika1997@gmail.com>
2022-02-24 14:17:15 -0300

    Part 2 - Vue implementation

---
+++ reactivity-vue/src/App.vue
@@ -0,0 +1,38 @@
+<script setup>
+import { ref, unref, computed } from "vue";
+</script>
+
+<script>
+let price = ref(100);
+let quantity = ref(500);
+const total = computed(() => price.value * quantity.value);
+
+function doubleQuantity() {
+  quantity.value = unref(quantity) * 2;
+}
+
+export default {
+  name: "app",
+};
+</script>
+<template>
+  <div id="app">
+    <div>
+      <h1>Vuejs</h1>
+      <h2>Price: {{ price }}</h2>
+      <button @click="price++">Increase Price by 1</button>
+      <h2>Quantity: {{ quantity }}</h2>
+      <button @click="doubleQuantity">Double Quantity</button>
+      <h2>Total: {{ total }}</h2>
+    </div>
+       <div>
+      <h1>Javascript</h1>
+      
+      <h2>Price: {{ price }}</h2>
+      <button @click="price++">Increase Price by 1</button>
+      <h2>Quantity: {{ quantity }}</h2>
+      <button @click="doubleQuantity">Double Quantity</button>
+      <h2>Total: {{ total }}</h2>
+    </div>
+  </div>
+</template>
